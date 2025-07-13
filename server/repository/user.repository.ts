2d060b635d1516ec6a7bfd0462182c4d5d0a8ddc
Tolojanahary <tolotor@yahoo.fr>
2025-07-13 22:30:50 +0300

    ➕ Feature: Server / Repositories / User repository / Checks if an email already exists

--- server/repository/user.repository.ts
+++ server/repository/user.repository.ts
@@ -29,6 +29,14 @@ export default class UserRepository {
     return (rows as { exists: number }[])[0].exists === 1;
   }
 
+  static async emailExists(email: string): Promise<boolean> {
+    const sql = `
+    SELECT EXISTS(SELECT 1 FROM ${tableName} WHERE email = ?) as 'exists'
+    `;
+    const [rows] = await getConnectionsPool().execute(sql, [email]);
+    return (rows as { exists: number }[])[0].exists === 1;
+  }
+
   static async create(payload: CreateUserDTO): Promise<ResultSetHeader> {
     const sql = `
     INSERT INTO ${tableName} (firstname, lastname, username, email, password) VALUES (?, ?, ?, ?, ?)
