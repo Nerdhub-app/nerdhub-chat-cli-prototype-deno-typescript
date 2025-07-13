98050cafb5e29692021a1cfc486951c288bffa56
Tolojanahary <tolotor@yahoo.fr>
2025-07-13 22:35:31 +0300

    ➕ Feature: CLI / APIs / User API / Check if an email already exists

--- cli/api/user.api.ts
+++ cli/api/user.api.ts
@@ -1,11 +1,19 @@
 import ApiFetch from "../helpers/api-fetch.helper.ts";
-import type { UsernameExistsResponsePayload } from "@scope/server/payload";
+import type {
+  CheckEmailExistsResponsePayload,
+  CheckUsernameExistsResponsePayload,
+} from "@scope/server/payload";
 
 export const USERS_ENDPOINT = "/users";
 
 export default class UserAPI {
   static usernameExists(username: string) {
     const endpoint = `${USERS_ENDPOINT}/username/${username}/exists`;
-    return ApiFetch.get<UsernameExistsResponsePayload>(endpoint);
+    return ApiFetch.get<CheckUsernameExistsResponsePayload>(endpoint);
+  }
+
+  static emailExists(email: string) {
+    const endpoint = `${USERS_ENDPOINT}/email/${email}/exists`;
+    return ApiFetch.get<CheckEmailExistsResponsePayload>(endpoint);
   }
 }
