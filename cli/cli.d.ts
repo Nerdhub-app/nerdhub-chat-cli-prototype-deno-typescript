f696bd12c950efe84ecd41d1b309f92c4a1c44b2
Tolojanahary <tolotor@yahoo.fr>
2025-07-16 00:45:30 +0300

    ➕ Feature: CLI / Types: More DB types

--- cli/cli.d.ts
+++ cli/cli.d.ts
@@ -6,30 +6,70 @@ import type {
 
 export type Nullable<T> = T | null;
 
+export type Replace<T, TKeys extends keyof T, TField> =
+  & Omit<T, TKeys>
+  & Record<TKeys, TField>;
+
+export type ReplaceRowTimestamps<
+  T extends Record<"created_at" | "updated_at", Date>,
+> = Replace<T, "created_at" | "updated_at", number>;
+
 export type User = UserWithoutPassword;
 
 export type E2EEParticipant = ServerE2EEParticipant;
 
+export type DbTableName =
+  | "e2ee_participant_onetime_prekeys"
+  | "e2ee_participant_prekey_bundles";
+
 export type E2EEParticipantPrekeyBundle = {
-  user_id: string;
+  user_id: number;
   pub_identity_key: Buffer;
   priv_identity_key: Buffer;
   pub_signed_prekey: Buffer;
-  pub_signed_prekey_signature: Buffer;
   priv_signed_prekey: Buffer;
+  signed_prekey_signature: Buffer;
   is_published: boolean;
-  participant_id: Nullable<string>;
+  participant_id: Nullable<number>;
   created_at: Date;
   updated_at: Date;
 };
+
+export type E2EEParticipantPrekeyBundleRow = ReplaceRowTimestamps<
+  Replace<
+    Replace<
+      E2EEParticipantPrekeyBundle,
+      | "pub_identity_key"
+      | "priv_identity_key"
+      | "pub_signed_prekey"
+      | "priv_signed_prekey",
+      Uint8Array
+    >,
+    "is_published",
+    number
+  >
+>;
 
 export type E2EEParticipantOnetimePrekey = {
   id: string;
   pub_key: Buffer;
   priv_key: Buffer;
   is_published: boolean;
-  user_id: string;
-  participant_id: Nullable<string>;
+  user_id: number;
+  participant_id: Nullable<number>;
   created_at: Date;
   updated_at: Date;
 };
+
+export type E2EEParticipantOnetimePrekeyRow = ReplaceRowTimestamps<
+  Replace<
+    Replace<
+      E2EEParticipantOnetimePrekey,
+      | "pub_key"
+      | "priv_key",
+      Uint8Array
+    >,
+    "is_published",
+    number
+  >
+>;
